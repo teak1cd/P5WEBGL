d8a997f375a993161ce75ca13e69251c61e8d097
teak1cd <isenne@sterneschool.org>
2016-11-08 23:45:47 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 
 var objects = [];
 var a = 0;
-
+var imag = [];
 function setup() {
   createCanvas(720, 720, WEBGL);
-    objects.push(new Box(0,0,0,200,"assets/dirt.png"));
-  // An array of Box objects
+    objects.push(new Box(0,0,0,200,0));
+  // An array of Box objects\
+  imag.push(loadImage("assets/dirt.png"));
   // Star with one
 }
 
@@ -19,6 +20,7 @@ function draw() {
   rotateY(a * 0.4);
   rotateZ(a * 0.1);
   for(var i = 0;i<objects.length;i++){
+      texture(imag[objects[i].texture])
       objects[i].show();
   }
   a+=0.01;
