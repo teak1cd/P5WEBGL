3afbda1dfb77e0d17d3392dce3e72673d857822c
teak1cd <isenne@sterneschool.org>
2016-11-09 14:27:40 -0800

    Update levels.js

--- levels.js
+++ levels.js
@@ -1,7 +1,7 @@
 function getLevels(n){
   switch(n){
     case 0:
-      return '{"textures":["tex.png"],"objects":[{"type":"Box","inputs":[0,0,0,100,100,100,0]},{"type":"Box","inputs":[0,200,0,200,200,200,0]}]}';
+      return '{"textures":["tex.png"],"objects":[{"type":"Box","inputs":[0,0,0,100,100,100,0]},{"type":"Box","inputs":[0,800,0,100,100,100,0]}]}';
     break;
   }
 }
