837d48524f77581f06de31718af5fceeb8276863
teak1cd <isenne@sterneschool.org>
2016-11-09 14:24:07 -0800

    Update and rename level0.json to levels.js

---
+++ levels.js
@@ -0,0 +1,7 @@
+function getLevels(n){
+  switch(n){
+    case 0:
+      return '{"textures":["tex.png"],"objects":[{"type":"Box","inputs":[0,0,0,200,200,200,0]},{"type":"Box","inputs":[0,300,0,200,200,200,0]}]}';
+    break;
+  }
+}
