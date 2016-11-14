ee81389fe58c6c4ce83c0eb7964a77df414d5749
teak1cd <isenne@sterneschool.org>
2016-11-14 14:34:26 -0800

    Update levels.js

--- levels.js
+++ levels.js
@@ -11,11 +11,11 @@ function getLevels(n){
         "inputs": [0, 800, 0, 300, 2]
     }, {
         "type": "Sphere",
-        "inputs": [0, 500, 0, 50, 50, 50, 1],
+        "inputs": [0, 500, 0, 50, 1],
         "promote":"hello_world"
     }, {
         "type": "Sphere",
-        "inputs": [0, 500, 0, 50, 50, 50, 1],
+        "inputs": [0, 500, 0, 50, 1],
         "promote":"hello_world2"
     }, {
         "type": "Box",
