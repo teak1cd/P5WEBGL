80970c814a47f2a956b37696cfb9944569879238
teak1cd <isenne@sterneschool.org>
2016-11-14 13:51:14 -0800

    Update level.js

--- level.js
+++ level.js
@@ -3,7 +3,7 @@ function level(){
     var output = [[],[]];
     for(var i = 0;i<data.objects.length;i++){
       if(typeof window[data.objects[i].type] !== "undefined"){
-        if(objects[i].promote){
+        if(typeof objects[i].promote!== "undefined"){
            window[objects[i].premote]=output.length;
         }
         output[0].push( new window[data.objects[i].type](data.objects[i].inputs));
